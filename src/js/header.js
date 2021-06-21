3512687a4f01ff458ca5f1c63e078a69db4db91f
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-06-21 21:14:50 +0300

    add-save-id

--- src/js/header.js
+++ src/js/header.js
@@ -28,3 +28,48 @@ function onLibraryClick(e) {
 
 
 };
+
+// Add to queue
+
+const addToQueueBtnRef = document.querySelector('[data-popup="backdrop"]')
+
+addToQueueBtnRef.addEventListener('click', onQueueClick);
+
+let itemsInQueue = JSON.parse(localStorage.getItem('queue'));
+
+if (JSON.parse(localStorage.getItem('queue')) === null) {
+    itemsInQueue = [];
+} else {
+    itemsInQueue = JSON.parse(localStorage.getItem('queue'))
+};
+
+
+function onQueueClick(e) {
+    const buttonQueue = document.querySelector('[data-queue]')
+    const elementId = buttonQueue.dataset.id;
+    const genreId = buttonQueue.dataset.genresid;
+
+    if (e.target !== buttonQueue) {
+        return;
+    }
+
+    itemsInQueue.push(elementId)
+    const uniqueItems = unique(itemsInQueue);
+    localStorage.setItem('queue', JSON.stringify(uniqueItems));
+
+};
+
+function unique(idItems) {
+    let result = [];
+
+    for (const id of idItems) {
+        if (!result.includes(id)) {
+            result.push(id);
+        }
+    }
+
+    return result;
+}
+
+
+console.log(JSON.parse(localStorage.getItem('queue')));
