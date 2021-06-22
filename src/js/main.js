8e92cbaa1141553563bb9e0d36aeaaeade70a77b
andriy-lanyk <LAM1988@ukr.net>
2021-06-22 23:14:25 +0300

    fixed input search

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,9 @@ import renderPage from './pagination';
 import debounce from 'lodash.debounce';
 import { API_KEY } from './API_KEY';
 import { onLoader, stopLoader } from './loader';
-import cardTpl from '../templates/film-card.hbs';
+
+const input = document.querySelector('.search-input');
+input.addEventListener('input', debounce(onInputMovieDetails, 400));
 
 const movieId = '10580';
 const mediaType = 'movie';
@@ -18,41 +20,27 @@ function startPageTrending(key, page) {
     totalMovies = movie.total_results;
     renderPage(totalMovies, numberOfPage);
   });
-
 }
 
 function onInputMovieDetails(e) {
-  const query = e.target.value.trim();
-  if (query.length < 1) {
-    onInputTrending();
-    return;
-  };
-  apiFetchRequest.fetchSearchMovie(query)
-    .then(movie => {
-      renderSection(movie.results);
-    })
-
+  query = e.target.value.toLowerCase().trim();
+  numberOfPage = 1;
+  // if (query.length < 1) {
+  //   onInputTrending();
+  //   return;
+  // };
+  apiFetchRequest.fetchSearchMovie(API_KEY, numberOfPage, query).then(movie => {
+    totalMovies = movie.total_results;
+    renderPage(totalMovies, numberOfPage, query);
+  });
 }
 
 function onInputMovie(id, key) {
-  onLoader()
+  onLoader();
   apiFetchRequest.fetchMovieDetails(id, key).then(movie => {
     console.log(movie);
-    stopLoader()
+    stopLoader();
   });
 }
 
 startPageTrending(API_KEY, numberOfPage);
-
-// onInputMovieDetails(API_KEY);
-// onInputMovie(movieId, API_KEY);
-const refs = {
-  galleryRef: document.querySelector('.js-gallery'),
-  searchInput: document.querySelector('.search-input'),
-}
-
-function renderSection(card) {
-  const markupCard = cardTpl(card);
-  refs.galleryRef.innerHTML = markupCard;
-}
-refs.searchInput.addEventListener('input', debounce(onInputMovieDetails, 500));
