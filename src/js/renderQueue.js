9e77090daeea9a3a6f8bbcb75e368144aa72c074
Данатей <danatey40793@gmail.com>
2021-06-24 19:02:46 +0300

    слил ветку

--- src/js/renderQueue.js
+++ src/js/renderQueue.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import { fetchMovieDetails } from './fetchRequests';
 import { onLoader, stopLoader } from './loader';
-import cardTmpl from '../templates/film-card.hbs';
-=======
-import { fetchTrending, fetchSearchMovie, fetchMovieDetails } from './fetchRequests';
-import renderPage from './pagination';
+// import renderPage from './pagination';
 import cardTpl from '../templates/film-card.hbs';
->>>>>>> header
+
 
 const elBtnQueue = document.querySelector('[data-queue-header');
 const listOfMovie = document.querySelector('.js-gallery');
@@ -18,19 +14,10 @@ function getQueueId() {
     const savedItems = JSON.parse(localStorage.getItem(qeryToGet));
 
     savedItems.forEach(id => {
-<<<<<<< HEAD
-        fetchMovieDetails(id).then(movie => {
-            console.log(movie)
+        fetchMovieDetails(id).then(data => {
             onLoader()
-            listOfMovie.innerHTML = cardTmpl(movie);
+            listOfMovie.innerHTML = cardTpl(data);
             stopLoader()
         })
-=======
-
-        fetchMovieDetails(id).then(data => {
-            galerryContRef.innerHTML = cardTpl(data);
-        })
-
->>>>>>> header
     })
 };
